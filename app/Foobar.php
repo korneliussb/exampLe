c64beff42886447d28e1bc7318ad35867b82650c
korneliussb <korneliussb@student.ce.undip.ac.id>
2020-05-10 00:23:03 +0700

    foobar

--- app/Foobar.php
+++ app/Foobar.php
@@ -3,11 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Foobar extends Model
 {
     public function name()
     {
-        return "laracast";
+        $val = ['one', 'two'];
+        if ($val) {
+            return 'good';
+        }
+
+        // $names = ['one', 'two', 'three'];
+        // return "laracast";
+
+
+        // return request()->redirect('/somewhere');
     }
 }
